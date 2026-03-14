577dccfd015d03bb012ead4d3e570f85f0d987a3
eugeniucc <eugeniucoloteniucc@gmail.com>
2026-03-14 23:54:45 +0200

    added sentry prisma rate/limit authjs

---
+++ sentry.server.config.ts
@@ -0,0 +1,18 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+import * as Sentry from '@sentry/nextjs'
+
+Sentry.init({
+  dsn: 'https://e6b29974da2c21d43d78e8be49427a63@o4511044607803392.ingest.de.sentry.io/4511044609638480',
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 0.1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true
+})
